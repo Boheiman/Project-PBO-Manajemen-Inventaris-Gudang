1e2fc919133fdfc3d3e30933bc976b06973cf21b
Aditya Bintang <adityabintang1812@gmail.com>
2024-12-27 13:57:47 +0700

    edit by Aditya Bintang

--- TransaksiMasuk.java
+++ TransaksiMasuk.java
@@ -1,6 +1,6 @@
 /**
  * Kelas untuk transaksi masuk yang mewarisi dari kelas Transaksi.
- * Konsep OOP:
+ * Konsep OOP: 
  * - Inheritance: Kelas ini mewarisi dari kelas Transaksi.
  * - Overriding: Metode prosesTransaksi() di-override untuk implementasi spesifik.
  */
