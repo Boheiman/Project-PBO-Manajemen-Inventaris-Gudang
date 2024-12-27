c7151a6bf916b50384616cb282f4d9a4e2c30f60
IlhamNurdin04 <149498782+IlhamNurdin04@users.noreply.github.com>
2024-12-27 14:14:16 +0700

    Update Barang.java

--- Barang.java
+++ Barang.java
@@ -1,14 +1,25 @@
+/**
+ * Kelas untuk barang.
+ * Konsep OOP:
+ * - Encapsulation: Fields adalah private dan diakses melalui public getters dan setters.
+ * - Constructor Overloading: Konstruktor mengambil parameter untuk menginisialisasi fields.
+ * - Overriding: Metode toString() di-override untuk representasi objek.
+ * - Aggregation: JenisBarang digunakan untuk mendefinisikan tipe barang.
+ */
+
 public class Barang {
     private String kodeBarang;
     private String namaBarang;
+    private JenisBarang jenisBarang; // New field
     private String kategori;
     private int stok;
     private double harga;
     private String pemasok;
 
-    public Barang(String kodeBarang, String namaBarang, String kategori, int stok, double harga, String pemasok) {
+    public Barang(String kodeBarang, String namaBarang, JenisBarang jenisBarang, String kategori, int stok, double harga, String pemasok) {
         this.kodeBarang = kodeBarang;
         this.namaBarang = namaBarang;
+        this.jenisBarang = jenisBarang; // Initialize new field
         this.kategori = kategori;
         this.stok = stok;
         this.harga = harga;
@@ -21,6 +32,9 @@ public class Barang {
     public String getNamaBarang() { return namaBarang; }
     public void setNamaBarang(String namaBarang) { this.namaBarang = namaBarang; }
 
+    public JenisBarang getJenisBarang() { return jenisBarang; } // Getter for jenisBarang
+    public void setJenisBarang(JenisBarang jenisBarang) { this.jenisBarang = jenisBarang; } // Setter for jenisBarang
+
     public String getKategori() { return kategori; }
     public void setKategori(String kategori) { this.kategori = kategori; }
 
@@ -32,4 +46,9 @@ public class Barang {
 
     public String getPemasok() { return pemasok; }
     public void setPemasok(String pemasok) { this.pemasok = pemasok; }
+
+    @Override
+    public String toString() {
+        return "Barang [kodeBarang=" + kodeBarang + ", namaBarang=" + namaBarang + ", jenisBarang=" + jenisBarang + ", kategori=" + kategori + ", stok=" + stok + ", harga=" + harga + ", pemasok=" + pemasok + "]";
+    }
 }
