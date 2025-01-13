544e59a14e26911f69bc1fcc140c63c9115eeb15
IlhamNurdin04 <149498782+IlhamNurdin04@users.noreply.github.com>
2025-01-13 16:17:47 +0700

    Delete ManajemenBarang.java

--- ManajemenBarang.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface ManajemenBarang {
-    void tambahBarang(Barang barang);
-    void perbaruiBarang(String kodeBarang, Barang barangBaru);
-    void hapusBarang(String kodeBarang);
-    ArrayList<Barang> daftarBarang();
-    
-}
